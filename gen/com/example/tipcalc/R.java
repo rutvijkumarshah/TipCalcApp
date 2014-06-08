0fa6e090e2315c4191533033a8b3b1867e24c052
rutvijashah <rutvijkumar.shah@gmail.com>
2014-06-08 16:39:56 -0700

    Improved UI first cut

--- gen/com/example/tipcalc/R.java
+++ gen/com/example/tipcalc/R.java
@@ -10,27 +10,172 @@ package com.example.tipcalc;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int apptheme_color=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int tab_host_default_height=0x7f050000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int apptheme_activated_background_holo_light=0x7f020000;
+        public static final int apptheme_btn_check_holo_light=0x7f020001;
+        public static final int apptheme_btn_check_off_disabled_focused_holo_light=0x7f020002;
+        public static final int apptheme_btn_check_off_disabled_holo_light=0x7f020003;
+        public static final int apptheme_btn_check_off_focused_holo_light=0x7f020004;
+        public static final int apptheme_btn_check_off_holo_light=0x7f020005;
+        public static final int apptheme_btn_check_off_pressed_holo_light=0x7f020006;
+        public static final int apptheme_btn_check_on_disabled_focused_holo_light=0x7f020007;
+        public static final int apptheme_btn_check_on_disabled_holo_light=0x7f020008;
+        public static final int apptheme_btn_check_on_focused_holo_light=0x7f020009;
+        public static final int apptheme_btn_check_on_holo_light=0x7f02000a;
+        public static final int apptheme_btn_check_on_pressed_holo_light=0x7f02000b;
+        public static final int apptheme_btn_default_disabled_focused_holo_light=0x7f02000c;
+        public static final int apptheme_btn_default_disabled_holo_light=0x7f02000d;
+        public static final int apptheme_btn_default_focused_holo_light=0x7f02000e;
+        public static final int apptheme_btn_default_holo_light=0x7f02000f;
+        public static final int apptheme_btn_default_normal_holo_light=0x7f020010;
+        public static final int apptheme_btn_default_pressed_holo_light=0x7f020011;
+        public static final int apptheme_btn_radio_holo_light=0x7f020012;
+        public static final int apptheme_btn_radio_off_disabled_focused_holo_light=0x7f020013;
+        public static final int apptheme_btn_radio_off_disabled_holo_light=0x7f020014;
+        public static final int apptheme_btn_radio_off_focused_holo_light=0x7f020015;
+        public static final int apptheme_btn_radio_off_holo_light=0x7f020016;
+        public static final int apptheme_btn_radio_off_pressed_holo_light=0x7f020017;
+        public static final int apptheme_btn_radio_on_disabled_focused_holo_light=0x7f020018;
+        public static final int apptheme_btn_radio_on_disabled_holo_light=0x7f020019;
+        public static final int apptheme_btn_radio_on_focused_holo_light=0x7f02001a;
+        public static final int apptheme_btn_radio_on_holo_light=0x7f02001b;
+        public static final int apptheme_btn_radio_on_pressed_holo_light=0x7f02001c;
+        public static final int apptheme_btn_rating_star_off_focused_holo_light=0x7f02001d;
+        public static final int apptheme_btn_rating_star_off_normal_holo_light=0x7f02001e;
+        public static final int apptheme_btn_rating_star_off_pressed_holo_light=0x7f02001f;
+        public static final int apptheme_btn_rating_star_on_focused_holo_light=0x7f020020;
+        public static final int apptheme_btn_rating_star_on_normal_holo_light=0x7f020021;
+        public static final int apptheme_btn_rating_star_on_pressed_holo_light=0x7f020022;
+        public static final int apptheme_btn_toggle_holo_light=0x7f020023;
+        public static final int apptheme_btn_toggle_off_disabled_focused_holo_light=0x7f020024;
+        public static final int apptheme_btn_toggle_off_disabled_holo_light=0x7f020025;
+        public static final int apptheme_btn_toggle_off_focused_holo_light=0x7f020026;
+        public static final int apptheme_btn_toggle_off_normal_holo_light=0x7f020027;
+        public static final int apptheme_btn_toggle_off_pressed_holo_light=0x7f020028;
+        public static final int apptheme_btn_toggle_on_disabled_focused_holo_light=0x7f020029;
+        public static final int apptheme_btn_toggle_on_disabled_holo_light=0x7f02002a;
+        public static final int apptheme_btn_toggle_on_focused_holo_light=0x7f02002b;
+        public static final int apptheme_btn_toggle_on_normal_holo_light=0x7f02002c;
+        public static final int apptheme_btn_toggle_on_pressed_holo_light=0x7f02002d;
+        public static final int apptheme_edit_text_holo_light=0x7f02002e;
+        public static final int apptheme_fastscroll_thumb_default_holo=0x7f02002f;
+        public static final int apptheme_fastscroll_thumb_holo=0x7f020030;
+        public static final int apptheme_fastscroll_thumb_pressed_holo=0x7f020031;
+        public static final int apptheme_ic_navigation_drawer=0x7f020032;
+        public static final int apptheme_item_background_holo_light=0x7f020033;
+        public static final int apptheme_list_activated_holo=0x7f020034;
+        public static final int apptheme_list_focused_holo=0x7f020035;
+        public static final int apptheme_list_longpressed_holo=0x7f020036;
+        public static final int apptheme_list_pressed_holo_light=0x7f020037;
+        public static final int apptheme_list_selector_background_transition_holo_light=0x7f020038;
+        public static final int apptheme_list_selector_disabled_holo_light=0x7f020039;
+        public static final int apptheme_list_selector_holo_light=0x7f02003a;
+        public static final int apptheme_progress_bg_holo_light=0x7f02003b;
+        public static final int apptheme_progress_horizontal_holo_light=0x7f02003c;
+        public static final int apptheme_progress_indeterminate_horizontal_holo_light=0x7f02003d;
+        public static final int apptheme_progress_primary_holo_light=0x7f02003e;
+        public static final int apptheme_progress_secondary_holo_light=0x7f02003f;
+        public static final int apptheme_progressbar_indeterminate_holo1=0x7f020040;
+        public static final int apptheme_progressbar_indeterminate_holo2=0x7f020041;
+        public static final int apptheme_progressbar_indeterminate_holo3=0x7f020042;
+        public static final int apptheme_progressbar_indeterminate_holo4=0x7f020043;
+        public static final int apptheme_progressbar_indeterminate_holo5=0x7f020044;
+        public static final int apptheme_progressbar_indeterminate_holo6=0x7f020045;
+        public static final int apptheme_progressbar_indeterminate_holo7=0x7f020046;
+        public static final int apptheme_progressbar_indeterminate_holo8=0x7f020047;
+        public static final int apptheme_rate_star_big_half_holo_light=0x7f020048;
+        public static final int apptheme_rate_star_big_off_holo_light=0x7f020049;
+        public static final int apptheme_rate_star_big_on_holo_light=0x7f02004a;
+        public static final int apptheme_rate_star_small_half_holo_light=0x7f02004b;
+        public static final int apptheme_rate_star_small_off_holo_light=0x7f02004c;
+        public static final int apptheme_rate_star_small_on_holo_light=0x7f02004d;
+        public static final int apptheme_ratingbar_full_empty_holo_light=0x7f02004e;
+        public static final int apptheme_ratingbar_full_filled_holo_light=0x7f02004f;
+        public static final int apptheme_ratingbar_full_holo_light=0x7f020050;
+        public static final int apptheme_ratingbar_holo_light=0x7f020051;
+        public static final int apptheme_ratingbar_small_holo_light=0x7f020052;
+        public static final int apptheme_scrubber_control_disabled_holo=0x7f020053;
+        public static final int apptheme_scrubber_control_focused_holo=0x7f020054;
+        public static final int apptheme_scrubber_control_normal_holo=0x7f020055;
+        public static final int apptheme_scrubber_control_pressed_holo=0x7f020056;
+        public static final int apptheme_scrubber_control_selector_holo_light=0x7f020057;
+        public static final int apptheme_scrubber_primary_holo=0x7f020058;
+        public static final int apptheme_scrubber_progress_horizontal_holo_light=0x7f020059;
+        public static final int apptheme_scrubber_secondary_holo=0x7f02005a;
+        public static final int apptheme_scrubber_track_holo_light=0x7f02005b;
+        public static final int apptheme_spinner_background_holo_light=0x7f02005c;
+        public static final int apptheme_spinner_default_holo_light=0x7f02005d;
+        public static final int apptheme_spinner_disabled_holo_light=0x7f02005e;
+        public static final int apptheme_spinner_focused_holo_light=0x7f02005f;
+        public static final int apptheme_spinner_pressed_holo_light=0x7f020060;
+        public static final int apptheme_switch_bg_disabled_holo_light=0x7f020061;
+        public static final int apptheme_switch_bg_focused_holo_light=0x7f020062;
+        public static final int apptheme_switch_bg_holo_light=0x7f020063;
+        public static final int apptheme_switch_inner_holo_light=0x7f020064;
+        public static final int apptheme_switch_thumb_activated_holo_light=0x7f020065;
+        public static final int apptheme_switch_thumb_disabled_holo_light=0x7f020066;
+        public static final int apptheme_switch_thumb_holo_light=0x7f020067;
+        public static final int apptheme_switch_thumb_pressed_holo_light=0x7f020068;
+        public static final int apptheme_switch_track_holo_light=0x7f020069;
+        public static final int apptheme_tab_indicator_holo=0x7f02006a;
+        public static final int apptheme_tab_selected_focused_holo=0x7f02006b;
+        public static final int apptheme_tab_selected_holo=0x7f02006c;
+        public static final int apptheme_tab_selected_pressed_holo=0x7f02006d;
+        public static final int apptheme_tab_unselected_focused_holo=0x7f02006e;
+        public static final int apptheme_tab_unselected_holo=0x7f02006f;
+        public static final int apptheme_tab_unselected_pressed_holo=0x7f020070;
+        public static final int apptheme_text_select_handle_left=0x7f020071;
+        public static final int apptheme_text_select_handle_middle=0x7f020072;
+        public static final int apptheme_text_select_handle_right=0x7f020073;
+        public static final int apptheme_textfield_activated_holo_light=0x7f020074;
+        public static final int apptheme_textfield_default_holo_light=0x7f020075;
+        public static final int apptheme_textfield_disabled_focused_holo_light=0x7f020076;
+        public static final int apptheme_textfield_disabled_holo_light=0x7f020077;
+        public static final int apptheme_textfield_focused_holo_light=0x7f020078;
+        public static final int ic_launcher=0x7f020079;
+        public static final int tipc_collapse=0x7f02007a;
+        public static final int tipc_expand=0x7f02007b;
     }
     public static final class id {
-        public static final int bill_amount_et=0x7f060000;
-        public static final int fifteen_percent=0x7f060003;
-        public static final int resultTextView=0x7f060005;
-        public static final int ten_percent=0x7f060002;
-        public static final int tips=0x7f060001;
-        public static final int twenty_percent=0x7f060004;
+        public static final int btn_down=0x7f080006;
+        public static final int btn_up=0x7f080004;
+        public static final int et_bill_amount=0x7f080002;
+        public static final int label_billAmount=0x7f080001;
+        public static final int label_perPerson=0x7f080009;
+        public static final int label_ppl=0x7f080007;
+        public static final int label_tip=0x7f080003;
+        public static final int label_tipAmount=0x7f08000a;
+        public static final int label_totalAmount=0x7f08000d;
+        public static final int label_valPerPerson=0x7f08000b;
+        public static final int label_valTipAmount=0x7f08000c;
+        public static final int label_valTipPercentage=0x7f080005;
+        public static final int label_valTotalAmount=0x7f08000e;
+        public static final int rd_tipExact=0x7f080013;
+        public static final int rd_tipUp=0x7f080011;
+        public static final int rd_tipdown=0x7f080012;
+        public static final int rdg_roundings=0x7f080010;
+        public static final int seekBar_ppl=0x7f080008;
+        public static final int textView1=0x7f08000f;
+        public static final int top_layout=0x7f080000;
     }
     public static final class layout {
         public static final int activity_tip_calc=0x7f030000;
+        public static final int apptheme_tab_indicator_holo=0x7f030001;
     }
     public static final class string {
-        public static final int HINT_EnterBillAmount=0x7f040001;
-        public static final int TXT_10_percent=0x7f040002;
-        public static final int TXT_15_percent=0x7f040003;
-        public static final int TXT_20_percent=0x7f040004;
-        public static final int TXT_TIP_TEMPLATE=0x7f040005;
-        public static final int app_name=0x7f040000;
+        public static final int HINT_EnterBillAmount=0x7f060001;
+        public static final int TXT_10_percent=0x7f060002;
+        public static final int TXT_15_percent=0x7f060003;
+        public static final int TXT_20_percent=0x7f060004;
+        public static final int TXT_SPLITS_TEMPLATE=0x7f060006;
+        public static final int TXT_TIP_TEMPLATE=0x7f060005;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
         /** 
@@ -54,10 +199,23 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
+        public static final int AutoCompleteTextViewAppTheme=0x7f070002;
+        public static final int ButtonAppTheme=0x7f070003;
+        public static final int ImageButtonAppTheme=0x7f070004;
+        public static final int ProgressBarAppTheme=0x7f070008;
+        public static final int RatingBarAppTheme=0x7f07000a;
+        public static final int RatingBarBigAppTheme=0x7f07000b;
+        public static final int RatingBarSmallAppTheme=0x7f07000c;
+        public static final int SeekBarAppTheme=0x7f070009;
+        public static final int SpinnerAppTheme=0x7f070005;
+        public static final int TabAppTheme=0x7f070006;
+        public static final int TabTextAppTheme=0x7f070007;
+        public static final int ToggleAppTheme=0x7f07000d;
+        public static final int _AppTheme=0x7f07000e;
     }
 }
--- src/com/example/tipcalc/TipCalcActivity.java
+++ src/com/example/tipcalc/TipCalcActivity.java
@@ -1,54 +1,94 @@
 package com.example.tipcalc;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 /***
  * 
  * #6a8eda
  * Launcher Main activity for Tip Calculator
+ * 
+ * 
  * @author Rutvijkumar Shah
  *
  */
-public class TipCalcActivity extends Activity {
+public class TipCalcActivity extends RoboActivity {
 
-	private TextView tipResultView;
-	private EditText billAmountEt;
-	private Float tipPercentage=0F;
-	private String tipResultTemplate;
-	
+
+	 @InjectView(R.id.et_bill_amount)      EditText billAmountEditText;
+	 @InjectView(R.id.label_valTipPercentage)      EditText percentageEditBox;
+	 @InjectView(R.id.label_ppl)              TextView splitWays;
+	 @InjectView(R.id.seekBar_ppl)            SeekBar seekBarSplits;
+	 @InjectView(R.id.label_valPerPerson)              TextView valPerPerson;
+	 @InjectView(R.id.label_valTipAmount)              TextView valTipAmount;
+	 @InjectView(R.id.label_valTotalAmount)              TextView valTotalAmount;
+	 
+	 private static String FONT="font_3838.ttf";
+	 
+	 private String splitTextTemplate=null;
+	 
+	 private static void applyTypeFace(Context ctx,ViewGroup layout,List<Integer> ids){
+		
+		 Typeface face = Typeface.createFromAsset(ctx.getAssets(), "fonts/"+FONT);
+		 int childcount = layout.getChildCount();
+		 for (int i=0; i < childcount; i++){
+		       View v = layout.getChildAt(i);
+		       if(v instanceof TextView ){
+		    	   ((TextView) v).setTypeface(face);
+		       }else if ( v instanceof  RadioButton){
+		    	   ((RadioButton) v).setTypeface(face);
+		       }
+		 }
+		    
+	 }
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tip_calc);
-		tipResultTemplate=getResources().getString(R.string.TXT_TIP_TEMPLATE);
-		tipResultView=(TextView)findViewById(R.id.resultTextView);
-		billAmountEt=(EditText)findViewById(R.id.bill_amount_et);
-		billAmountEt.addTextChangedListener(new TextWatcher() {
+	
+		applyTypeFace(this, (RelativeLayout)findViewById(R.id.top_layout), new ArrayList<Integer>());
+		
+		this.splitTextTemplate=getResources().getString(R.string.TXT_SPLITS_TEMPLATE);
+		seekBarSplits.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			
 			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-					//Update text view based on bill amount changes
-					//this will use last tip percentage selection
-					updateTipView(tipPercentage);
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-				//DO NOTHING
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
-			public void afterTextChanged(Editable s) {
-				//DO NOTHING
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				splitWays.setText(String.format(splitTextTemplate, progress));
 			}
 		});
+		
+		
+
 	}
 
 	/***
@@ -59,27 +99,8 @@ public class TipCalcActivity extends Activity {
 	public void calcuateTip(final View v){
     	final int viewId=v.getId();
     	float tipPercentage=Float.parseFloat(v.getTag().toString());
-    	//Store selected percentage
-    	this.tipPercentage=tipPercentage;
-    	updateTipView(tipPercentage);
     }
-	/***
-	 * Update Tip View for given tip percentage
-	 * @param tipPercentage
-	 */
-	private void updateTipView(float tipPercentage) {
-		//Clears Tip text
-		tipResultView.setText("");
-		String billAmountStr=billAmountEt.getText().toString();
-		
-		//Do not show Tip calculated when there is no input or no tip button pressed
-		if(billAmountStr != null && !billAmountStr.isEmpty() && tipPercentage !=0 ){
-			float billAmount=Float.parseFloat(billAmountStr);
-	    	float tip=tipCalc(billAmount, tipPercentage);
-	    	tipResultView.setText(String.format(tipResultTemplate, tip));
-		}
-    
-	}
+	
 	
 	/***
 	 * Calculates tip for given bill amount and tip percentage.
